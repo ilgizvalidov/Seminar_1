8dcabca325410a196908885aba0a50ca6d225e51
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-04 20:29:20 +0300

    программа проверят является ли первое число квадратом второго. числа вводятся пользователем с клавиатуры

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.WriteLine("Введите первое чило: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе чило: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2*number2) Console.WriteLine ("да");
+else Console.WriteLine ("нет");
