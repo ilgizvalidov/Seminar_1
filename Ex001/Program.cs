7753ada84a25020982677e804f9a183e59162b89
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-04 20:10:26 +0300

    нахождение квадрата числа вводимого пользователем

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите целое число: ");
+int number = Convert.ToInt32 (Console.ReadLine () );
+int result = number * number;
+Console.WriteLine ("Квадратцелого числа " + number + " равен " + result);
+if (number > 0 )
+{
+    Console.WriteLine ("Число положительное");
+}
+else if (number < 0) 
+        {
+            Console.WriteLine ("Число отрицательное");
+        }
+     else 
+        {
+            Console.WriteLine ("Вы ввели ноль");
+        }
