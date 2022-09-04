fe883a48b96ad989fd55ba23261d4f32bbd43db4
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-04 20:43:13 +0300

    программа выводит день недели по заданному номеру

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите чило от 1 до 7");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+if (number == 1 ) Console.WriteLine ("понедельник");
+if (number == 2 ) Console.WriteLine ("вторник");
+if (number == 3 ) Console.WriteLine ("среда");
+if (number == 4 ) Console.WriteLine ("четверг");
+if (number == 5 ) Console.WriteLine ("рятница");
+if (number == 6 ) Console.WriteLine ("субббота");
+if (number == 7 ) Console.WriteLine ("воскресенье");
