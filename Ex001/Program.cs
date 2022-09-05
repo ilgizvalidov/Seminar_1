904ce90c653cd5a77a8e96688d1efc1c57f22b5d
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-05 16:20:32 +0300

    познакомились с циклом while

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -7,16 +7,19 @@
         if (number > 0 )
             Console.WriteLine ("Число положительное");
             
-        else if (number < 0) 
-                   
-            Console.WriteLine ("Число отрицательное");
-                    
-            else 
-                  Console.WriteLine ("Вы ввели ноль");
-                    
-        }
-    catch
+        else if (number < 0) Console.WriteLine ("Число отрицательное");
+            else Console.WriteLine ("Вы ввели ноль"); 
+// Выведу все числа от 0 до number
+            int i = 0;
+            while (i <= number) 
+                {
+                    Console.Write(i + ", ");
+                    i++;            
+                }
 
+        }
+
+    catch
         {
             Console.WriteLine ("надо было вводить именно целое число");
         }
