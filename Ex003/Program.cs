b10a7fcf81f2ee6943b08e22035e998bcf416b85
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-05 16:08:45 +0300

    познакомились с switch case

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,10 +1,21 @@
 ﻿Console.WriteLine ("Введите чило от 1 до 7");
 int number = Convert.ToInt32 (Console.ReadLine());
-
-if (number == 1 ) Console.WriteLine ("понедельник");
-if (number == 2 ) Console.WriteLine ("вторник");
-if (number == 3 ) Console.WriteLine ("среда");
-if (number == 4 ) Console.WriteLine ("четверг");
-if (number == 5 ) Console.WriteLine ("рятница");
-if (number == 6 ) Console.WriteLine ("субббота");
-if (number == 7 ) Console.WriteLine ("воскресенье");
+switch (number)
+{
+    case 1: Console.WriteLine ("понедельник");
+        break;
+    case 2: Console.WriteLine ("вторник");
+        break;
+    case 3: Console.WriteLine ("среда"); 
+        break;
+    case 4: Console.WriteLine ("четверг");
+        break;
+    case 5: Console.WriteLine ("пятница");
+        break;
+    case 6: Console.WriteLine ("суббота");
+        break;
+    case 7: Console.WriteLine ("воскресенье");
+        break;
+    default: Console.WriteLine ("введите привильное число");
+        break;                       
+}
