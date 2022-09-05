1bab8ff195c25deb2c6db450a303663cc2961070
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-05 15:53:08 +0300

    добавили конструкцию try {} catch {}

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,13 +1,13 @@
 ﻿try 
-    {
+{
     Console.WriteLine("Введите первое чило: ");
     int number1 = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Введите второе чило: ");
     int number2 = Convert.ToInt32(Console.ReadLine());
     if (number1 == number2*number2) Console.WriteLine ("Число " + number1 + " является квадратом числа " + number2 );
     else if (number2 == number1 * number1) Console.WriteLine ("Число " + number2 + " является квадратом числа " + number1);
-            else Console.WriteLine ("числа не являются квадратами друг друга");
-    }
+         else Console.WriteLine ("числа не являются квадратами друг друга");
+}
 catch
 {
     Console.WriteLine ("Вы ввели не числа");
