4c2fba6e3f261e65e460373573caac8eeb805361
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-05 17:36:33 +0300

    программа принимает целое положительное ртрехзначное чило и выводит первую цифру

--- Ex005/Program.cs
+++ Ex005/Program.cs
@@ -3,7 +3,7 @@
     Console.Write("Введите трехзначное целое положительное число : ");
     int number = Convert.ToInt32(Console.ReadLine());
         if (number < 1000 ) 
-            if (number > 99) Console.WriteLine (number % 10); // остаток от деления числа на 10 и будет последней цифрой
+            if (number > 99) Console.WriteLine (number / 100); // получаем первую цифру от трехзначного числа
             else Console.WriteLine ("Вы ввели не трехзначное число");  
         else Console.WriteLine ("Вы ввели число более чем с тремя знаками");    
 }
