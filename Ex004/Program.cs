b08413d0de7b2ce50efedff0121b0005e270acdd
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-05 16:44:01 +0300

    программа выводит числа от - N до N. N вводится пользователем с клавиатуры

--- Ex004/Program.cs
+++ Ex004/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try 
+{
+Console.Write ("Введите целое число : ");
+int number = Convert.ToInt32 (Console.ReadLine());
+ 
+    if (number > 0 )
+    {
+    int i = (0 - number);
+    while (i <= number)
+    {
+        Console.Write (i + ", ");
+        i++;
+    }
+    }
+
+    else Console.WriteLine ("Вы ввели отрицательное значение"); 
+}
+
+catch
+
+{
+    Console.Write ("Вы ввели не число");
+}
