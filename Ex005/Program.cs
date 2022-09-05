b8fa89e8534a049dca9ef0a493dd72885ba0a053
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-05 17:16:41 +0300

    программа принимает целое положительное ртрехзначное чило и выводит последнюю цифру

--- Ex005/Program.cs
+++ Ex005/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try 
+{
+    Console.Write("Введите трехзначное целое положительное число : ");
+    int number = Convert.ToInt32(Console.ReadLine());
+        if (number < 1000 ) 
+            if (number > 99) Console.WriteLine (number % 10); // остаток от деления числа на 10 и будет последней цифрой
+            else Console.WriteLine ("Вы ввели не трехзначное число");  
+        else Console.WriteLine ("Вы ввели число более чем с тремя знаками");    
+}
+catch 
+{
+    Console.WriteLine ("Вы ввели не трехзначное целое число, повторите попытку");
+}
